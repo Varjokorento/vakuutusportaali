9e4c98689048e86837cc4a9c4a7fce38745ab81c
joonvena@gmail.com <joonvena@gmail.com>
2018-08-27 19:51:34 +0300

    UI modifications

--- src/components/page_insurances.js
+++ src/components/page_insurances.js
@@ -31,88 +31,83 @@ class InsuranceIndex extends Component {
             <div>
                 <Grid fluid className="splash">
                     <Row className="show-grid text-center information">
-                        <Col xs={12} sm={12}>
+
                         <h3>Valitse vakuutustyyppi</h3>
                         <DropdownButton
                                 title={"Ajoneuvovakuutukset"}
                                 key={"avain"}
                                 className="insurance_selector"
-                                id={this.insurances.id}
+                                id="vain"
                                 >
-                        <MenuItem eventKey="1">{this.insurances}</MenuItem>
-                        <MenuItem eventKey="2">Another action</MenuItem>
-                        <MenuItem eventKey="3">Active Item</MenuItem>
+                        <MenuItem eventKey="1">Henkilöauto</MenuItem>
+                        <MenuItem eventKey="2">Pakettiauto</MenuItem>
+                        <MenuItem eventKey="3">Moottoripyörä</MenuItem>
+                        <MenuItem eventKey="4">Mopo</MenuItem>
+                        <MenuItem eventKey="5">Matkailuauto</MenuItem>
+                        <MenuItem eventKey="6">Matkailuvaunu</MenuItem>
+                        <MenuItem eventKey="7">Moottorikelkka</MenuItem>
+                        <MenuItem eventKey="8">Mönkijä</MenuItem>
+                        <MenuItem eventKey="9">Perävaunu</MenuItem>
+                        <MenuItem eventKey="10">Vene</MenuItem>
+                        <MenuItem eventKey="11">Traktori</MenuItem>
+                        <MenuItem eventKey="12">Kuorma-auto</MenuItem>
+                        <MenuItem eventKey="13">Museoajoneuvo</MenuItem>
+                        <MenuItem eventKey="14">Auton kuntoturva</MenuItem>
                         </DropdownButton>
 
                         <DropdownButton
-                                title="Kotivakuutukset"
+                                title="Koti & omaisuus"
                                 key={"avain"}
                                 className="insurance_selector"
                                 >
-                        <MenuItem eventKey="1">Action</MenuItem>
-                        <MenuItem eventKey="2">Another action</MenuItem>
-                        <MenuItem eventKey="3">Active Item</MenuItem>
+                        <MenuItem eventKey="1">Kerrostalo</MenuItem>
+                        <MenuItem eventKey="2">Rivitalo</MenuItem>
+                        <MenuItem eventKey="3">Omakotitalo</MenuItem>
+                        <MenuItem eventKey="4">Paritalo</MenuItem>
+                        <MenuItem eventKey="5">Erillistalo</MenuItem>
+                        <MenuItem eventKey="6">Mökki</MenuItem>
+                        <MenuItem eventKey="7">Sijoitusasunto</MenuItem>
+                        <MenuItem eventKey="8">Metsä</MenuItem>
+                        <MenuItem eventKey="9">Vene</MenuItem>
                         </DropdownButton>
 
                         <DropdownButton
-                                title="Henkilövakuutukset"
+                                title="Matka"
                                 key={"avain"}
                                 className="insurance_selector"
                                 >
-                        <MenuItem eventKey="1">Action</MenuItem>
-                        <MenuItem eventKey="2">Another action</MenuItem>
-                        <MenuItem eventKey="3">Active Item</MenuItem>
+                        <MenuItem eventKey="1">Matkavakuutus</MenuItem>
                         </DropdownButton>
-                        </Col>
+                        
 
                         <DropdownButton
-                                title="Nuorisovakuutus"
+                                title="Henkilöt"
                                 key={"avain"}
                                 className="insurance_selector"
                                 >
-                        <MenuItem eventKey="1">Action</MenuItem>
-                        <MenuItem eventKey="2">Another action</MenuItem>
-                        <MenuItem eventKey="3">Active Item</MenuItem>
-                        </DropdownButton>
-
-                        <DropdownButton
-                                title="Matkavakuutus"
-                                key={"avain"}
-                                className="insurance_selector"
-                                >
-                        <MenuItem eventKey="1">Action</MenuItem>
-                        <MenuItem eventKey="2">Another action</MenuItem>
-                        <MenuItem eventKey="3">Active Item</MenuItem>
+                        <MenuItem eventKey="1">Vauva(raskaus)</MenuItem>
+                        <MenuItem eventKey="2">Lapsi(7 vrk - 14 v)</MenuItem>
+                        <MenuItem eventKey="3">Aikuinen</MenuItem>
+                        <MenuItem eventKey="4">Henkivakuutus</MenuItem>
+                        <MenuItem eventKey="5">Urheiluvakuutus</MenuItem>
                         </DropdownButton>
 
                         <DropdownButton
-                                title="Eläinvakuutukset"
+                                title="Eläimet"
                                 key={"avain"}
                                 className="insurance_selector"
                                 >
-                        <MenuItem eventKey="1">Action</MenuItem>
-                        <MenuItem eventKey="2">Another action</MenuItem>
-                        <MenuItem eventKey="3">Active Item</MenuItem>
+                        <MenuItem eventKey="1">Koira</MenuItem>
+                        <MenuItem eventKey="2">Kissa</MenuItem>
+                        <MenuItem eventKey="3">Hevonen</MenuItem>
                         </DropdownButton>
 
                         <DropdownButton
-                                title="Venevakuutus"
+                                title="Nuorisopaketti"
                                 key={"avain"}
                                 className="insurance_selector"
                                 >
-                        <MenuItem eventKey="1">Action</MenuItem>
-                        <MenuItem eventKey="2">Another action</MenuItem>
-                        <MenuItem eventKey="3">Active Item</MenuItem>
-                        </DropdownButton>
-
-                        <DropdownButton
-                                title="Metsävakuutus"
-                                key={"avain"}
-                                className="insurance_selector"
-                                >
-                        <MenuItem eventKey="1">Action</MenuItem>
-                        <MenuItem eventKey="2">Another action</MenuItem>
-                        <MenuItem eventKey="3">Active Item</MenuItem>
+                        <MenuItem eventKey="1">Nuorisopaketti (18 - 28v)</MenuItem>
                         </DropdownButton>
                     </Row>
                 </Grid>
