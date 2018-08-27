0c578dde3b5fc6bb353033a0e206fb63f0ec4543
joonvena@gmail.com <joonvena@gmail.com>
2018-08-27 17:06:41 +0300

    Added dropdown

--- src/components/page_insurances.js
+++ src/components/page_insurances.js
@@ -3,6 +3,28 @@ import { Link } from 'react-router-dom';
 import { Grid, Row, Col, DropdownButton, MenuItem } from 'react-bootstrap';
 
 class InsuranceIndex extends Component {
+    constructor() {
+        super();
+        this.state = {
+            insurances: [],
+        };
+    }
+
+    componentDidMount() {
+        let initialInsurances = [];
+        fetch('http://localhost:4000/insurancetypes')
+            .then(response => {
+                return response.json();
+            }).then(insurance => {
+                initialInsurances = insurance.results.map((insurance) => {
+                    return insurance
+                });
+                console.log(initialInsurances);
+                this.setState({
+                    insurances: initialInsurances,
+                });
+            });
+    }
 
     render() {
         return (
@@ -12,11 +34,12 @@ class InsuranceIndex extends Component {
                         <Col xs={12} sm={12}>
                         <h3>Valitse vakuutustyyppi</h3>
                         <DropdownButton
-                                title="Ajoneuvovakuutukset"
+                                title={"Ajoneuvovakuutukset"}
                                 key={"avain"}
                                 className="insurance_selector"
+                                id={this.insurances.id}
                                 >
-                        <MenuItem eventKey="1">Action</MenuItem>
+                        <MenuItem eventKey="1">{this.insurances}</MenuItem>
                         <MenuItem eventKey="2">Another action</MenuItem>
                         <MenuItem eventKey="3">Active Item</MenuItem>
                         </DropdownButton>
