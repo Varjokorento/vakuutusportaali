5931ca29b9193facff5b29a4a03a47d7bb0d8981
JunttilaJoonas <junttila.joonas@gmail.com>
2018-08-28 10:28:52 +0300

    postaus toimii täysin

--- vakuutuspalvelin/routes/insuranceRoute.js
+++ vakuutuspalvelin/routes/insuranceRoute.js
@@ -19,30 +19,29 @@ router.get("/", (req, res) => {
 
 router.post("/", (req, res) => {
     let insurance = req.body;
-    console.log("Insurance to be created:",insurance);
+    console.log("Insurance to be created:", insurance);
     insuranceService.addOne(insurance)
-        .then((result, error) => {
-            if (result) {
-                res.status(201);
-                res.send(result);
+        .then(result => {
+            if (result.errors) {
+                res.status(400);
             } else {
                 res.status(201);
-                res.send("Error!");
             }
+            res.send(result);
         });
 });
 
 router.put("/", (req, res) => {
-   let insurance = req.body;
-   console.log("Insurance to be updated:", insurance);
-   insuranceService.updateOneById(req.body);
-   res.status(200);
-   res.send("Updated");
+    let insurance = req.body;
+    console.log("Insurance to be updated:", insurance);
+    insuranceService.updateOneById(req.body);
+    res.status(200);
+    res.send("Updated");
 });
 
 router.delete("/:id", (req, res) => {
-   res.status(200);
-   res.send("Deleted");
+    res.status(200);
+    res.send("Deleted");
 });
 
 module.exports = router;
