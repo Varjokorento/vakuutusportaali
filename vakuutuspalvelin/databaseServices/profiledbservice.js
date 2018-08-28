2622a0ed3aa759f705c107a97fc16566f912192e
JunttilaJoonas <junttila.joonas@gmail.com>
2018-08-28 15:40:47 +0300

    testejä

---
+++ vakuutuspalvelin/databaseServices/profiledbservice.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const Profile = require('../models/profile');
+
+function findAll() {
+    //findAllProfile
+}
+
+function findOneById(req, res, next) {
+    let email = req.user.email
+    Profile.findOne({email: email})
+        .populate("profilesinsurances")
+        .then(profile => res.json(profile));
+}
+
+function updateOneById(req, res) {
+
+}
+
+function deleteOneById(req, res) {
+
+}
+
+function AddInsuranceToACustomer(req, res) {
+    /* The following should happen: 1) Insurance is created and added to the database. 2) InsuranceID
+    is then set into the profile.*/
+
+    Profile.findByIdandUpdate({_id: req.body.id}, req.body, (err, profile) => {
+        res.send("Profile updated")
+    });
+}
+
+function AddProfile(req, res) {
+    Profile.create(req.body)
+        .then(res.send("Profile created"));
+}
+
+function deleteProfile(req, res) {
+    Profile.deleteOneById({_id: req.body.id}, (err, profile) => {
+        res.send('Profile deleted')
+    })
+}
+
+// here all kinds of features: update profile, update one part of a profile, delete profile ...
+
+module.exports = {AddProfile, findAll, updateOneById, deleteOneById, findOneById, AddInsuranceToACustomer}
